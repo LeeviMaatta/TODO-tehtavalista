24d6c24e53729069843acefa787ee5ce67a02745
LeeviMaatta <lepa2505@gmail.com>
2025-11-03 15:15:25 +0200

    1-6 valmis

--- server/routes/todoRouter.js
+++ server/routes/todoRouter.js
@@ -46,39 +46,4 @@ router.delete('/delete/:id', (req, res, next) => {
         })
 })
 
-router.post('/signin', (req, res, next) => {
-    const { user } = req.body
-
-    if (!user || !user.email || !user.password) {
-        const error = new Error('Email and password are required')
-        error.status = 400
-        return next(error)
-    }
-
-    pool.query('SELECT * FROM account WHERE email = $1', [user.email], (err, result) => {
-        if (err) return next(err)
-        if (result.rows.length === 0) {
-            const error = new Error('User not found')
-            error.status = 404
-            return next(error)
-        }
-        const dbUser = result.rows[0]
-        compare(user.password, dbUser.password, (err, isMatch) => {
-            if (err) return next(err)
-
-            if (!isMatch) {
-                const error = new Error('Invalid password')
-                error.status = 401
-                return next(error)
-            }
-        })
-        const token = sign({ user: dbUser.email }, process.env.JWT_SECRET)
-        res.status(200).json({
-            id: dbUser.id,
-            email: dbUser.email,
-            token
-        })
-    })
-})
-
 export default router
